fa208a3c9d66544aea8344b234acb2dcb6f3e809
Voidoo <84565530+Hex-Zhou@users.noreply.github.com>
2022-05-04 23:38:47 +0800

    終極完成版

--- js/earthquake.js
+++ js/earthquake.js
@@ -18,7 +18,7 @@ const height = 1024;
 // 畫布x軸 0~1023
 let X_now = 0;
 // 速度參數 數字越大，畫布呈現範圍越大，波型越擠
-let x1_speed = 16;
+let x1_speed = 128;
 ctx.fillStyle = "rgb(3,59,80)";
 ctx.strokeStyle = "rgb(1,255,241)";
 ctx.lineWidth = 2;
@@ -33,7 +33,6 @@ var analyser1;
 var analyser2;
 var splitter;
 var javascriptNode;
-
 // Dom代稱
 var body = document.getElementsByTagName("body");
 var scope = document.getElementById("scope");
@@ -43,28 +42,22 @@ var Mouse_Down = false; //滑鼠是否按著
 var start = false;
 var posX_Down = 0;
 var now_at = 0;
-let timeToggle = true;
-let counterFilter = 0;
-
 scope.addEventListener("mousedown", (e) => {
   Mouse_Down = true;
   posX_Down = e.offsetX;
 });
-
 scope.addEventListener("mouseup", () => {
   Mouse_Down = false;
 });
 scope.addEventListener("mouseout", () => {
   Mouse_Down = false;
 });
-
 scope.addEventListener("mousemove", (e) => {
   if (Mouse_Down) {
     should_we_STOP = true;
     // numbers = (posX_Down - e.offsetX) / scope.getBoundingClientRect().width / 3;
     // now_at = now_at + x1_speed * numbers;
     let pos_move = e.offsetX;
-
     if (x1_speed === 4) {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 8 : now_at - 1 / 8;
     } else if (x1_speed === 2) {
@@ -73,17 +66,17 @@ scope.addEventListener("mousemove", (e) => {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 4 : now_at - 1 / 4;
     } else if (x1_speed === 1) {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 4 : now_at - 1 / 4;
-    } else {
-      now_at = posX_Down - pos_move > 0 ? now_at + x1_speed / 32 : now_at - x1_speed / 32;
+    } else if (x1_speed >= 4) {
+      now_at = posX_Down - pos_move > 0 ? now_at + x1_speed / 16 : now_at - x1_speed / 16;
+    } else if (x1_speed >= 64) {
+      now_at = posX_Down - pos_move > 0 ? now_at + x1_speed / 16 : now_at - x1_speed / 16;
     }
     posX_Down = pos_move;
     now_at = now_at < 0 ? 0 : now_at;
     now_at = now_at > database.length ? database.length : now_at;
-
     DBDraw(Math.floor(now_at));
   }
 });
-
 scope.addEventListener("touchstart", (e) => {
   Mouse_Down = true;
   posX_Down = e.touches[0].clientX;
@@ -95,7 +88,6 @@ scope.addEventListener("touchmove", (e) => {
   if (Mouse_Down) {
     should_we_STOP = true;
     let pos_move = e.touches[0].clientX;
-
     if (x1_speed === 4) {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 8 : now_at - 1 / 8;
     } else if (x1_speed === 2) {
@@ -104,13 +96,14 @@ scope.addEventListener("touchmove", (e) => {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 4 : now_at - 1 / 4;
     } else if (x1_speed === 1) {
       now_at = posX_Down - pos_move > 0 ? now_at + 1 / 4 : now_at - 1 / 4;
-    } else {
+    } else if (x1_speed >= 4) {
       now_at = posX_Down - pos_move > 0 ? now_at + x1_speed / 16 : now_at - x1_speed / 16;
+    } else if (x1_speed >= 64) {
+      now_at = posX_Down - pos_move > 0 ? now_at + x1_speed / 32 : now_at - x1_speed / 32;
     }
     posX_Down = pos_move;
     now_at = now_at < 0 ? 0 : now_at;
     now_at = now_at > database.length ? database.length : now_at;
-
     DBDraw(Math.floor(now_at));
   }
 });
@@ -118,39 +111,30 @@ scope.addEventListener("touchmove", (e) => {
 function buttom_div_width() {
   return document.getElementById("buttom_div").getBoundingClientRect().width;
 }
-
 function buttom_div_height() {
   return document.getElementById("buttom_div").getBoundingClientRect().height;
 }
 function lil_ctn_real_left() {
   return document.getElementById("lil_ctn").getBoundingClientRect().left - document.getElementById("buttom_div").getBoundingClientRect().left;
 }
-
 //跑流程
-
 function connectAudioAPI() {
   if (checkScreen() && !start) {
     try {
       start = true;
       context = new AudioContext();
-
       analyser1 = context.createAnalyser();
       analyser1.fftSize = 4096;
-
       analyser2 = context.createAnalyser();
       analyser2.fftSize = 4096;
-
       splitter = context.createChannelSplitter();
-
       javascriptNode = context.createScriptProcessor(2048, 1, 1);
-
       connectInput();
     } catch (e) {
       alert(e);
     }
   }
 }
-
 function connectInput() {
   navigator.mediaDevices
     .getUserMedia({ audio: true })
@@ -161,23 +145,20 @@ function connectInput() {
       alert("In connectInput : " + e);
     });
 }
-
 function gotStream(stream) {
   try {
     mediaSource = context.createMediaStreamSource(stream);
-
     mediaSource.connect(splitter);
-
     splitter.connect(analyser1, 0, 0);
     splitter.connect(analyser2, 1, 0);
-
     resizeCanvas();
     clearCTX();
+
     javascriptNode.connect(context.destination);
 
     javascriptNode.onaudioprocess = function () {
       // 接收到音頻訊號後，暫停/繼續?
-      if (!should_we_STOP && timeToggle) {
+      if (!should_we_STOP) {
         GetData();
       }
     };
@@ -186,27 +167,18 @@ function gotStream(stream) {
   }
 }
 
-setInterval(() => {
-  // timeToggle = !timeToggle;
-  timeToggle = true;
-}, 10);
-
 var database = [];
-
 function GetData() {
-  counterFilter++;
-  if (counterFilter % 4 == 0) {
-    var timeData1 = new Uint8Array(analyser1.frequencyBinCount);
-    // 將數據 存入 數據陣列 以備未來使用
-    ChecklineWidth();
-    database.push(timeData1);
-    analyser1.getByteTimeDomainData(timeData1);
-    // 取得音頻數據 後 繪製最新數據
-    now_at = database.length;
-    ContiDraw(now_at);
-  }
-}
+  var timeData1 = new Uint8Array(analyser1.frequencyBinCount);
+  // 將數據 存入 數據陣列 以備未來使用
+  ChecklineWidth();
 
+  database.push(timeData1);
+  analyser1.getByteTimeDomainData(timeData1);
+  // 取得音頻數據 後 繪製最新數據
+  now_at = database.length;
+  ContiDraw(now_at);
+}
 window.addEventListener("resize", resizeCanvas);
 function resizeCanvas() {
   let canvas_cnt = document.getElementsByClassName("canvas_cnt")[0];
@@ -214,11 +186,10 @@ function resizeCanvas() {
   document.getElementById("scope").style.height = canvas_cnt.getBoundingClientRect().height + "px";
   checkScreen();
 }
-
 function clearCTX() {
+  ctx.beginPath();
   ctx.drawImage(background, 0, 0, 1024, 1024);
 }
-
 function ContiDraw(xxx) {
   var timeData1 = database[xxx - 1];
 
@@ -226,15 +197,17 @@ function ContiDraw(xxx) {
     clearCTX();
     X_now = 0;
   }
-  ctx.beginPath();
 
+  ctx.lineTo(X_now, timeData1[0] * 4);
   for (var x = 0; x < 1024; x++) {
-    ctx.lineTo(X_now + x / x1_speed, timeData1[x] * 4);
+    if (x % 512 == 0) {
+      ctx.lineTo(X_now + x / x1_speed, timeData1[x] * 4);
+    }
   }
+
   X_now = X_now + 1024 / x1_speed;
   ctx.stroke();
 }
-
 function DBDraw(xxx) {
   if (xxx - x1_speed < 0) {
   } else {
@@ -245,27 +218,27 @@ function DBDraw(xxx) {
         clearCTX();
         X_now = 0;
       }
-      ctx.beginPath();
+
+      ctx.lineTo(X_now, timeData1[0] * 4);
       for (var x = 0; x < 1024; x++) {
-        ctx.lineTo(X_now + x / x1_speed, timeData1[x] * 4);
+        if (x % 512 == 0) {
+          ctx.lineTo(X_now + x / x1_speed, timeData1[x] * 4);
+        }
       }
       X_now = X_now + 1024 / x1_speed;
       ctx.stroke();
     }
   }
 }
-
 function btn_stop() {
   should_we_STOP = true;
 }
-
 function btn_go() {
   should_we_STOP = false;
   if (!start) {
     connectAudioAPI();
   }
 }
-
 function speed_fast() {
   if (x1_speed * 2 < 512) {
     x1_speed *= 2;
@@ -274,7 +247,6 @@ function speed_fast() {
   ChecklineWidth();
   DBDraw(Math.floor(now_at));
 }
-
 function speed_slow() {
   if (x1_speed / 2 > 1 / 4) {
     x1_speed /= 2;
@@ -283,7 +255,6 @@ function speed_slow() {
   ChecklineWidth();
   DBDraw(Math.floor(now_at));
 }
-
 function checkScreen() {
   let width = body[0].getBoundingClientRect().width;
   let height = body[0].getBoundingClientRect().height;
@@ -294,10 +265,8 @@ function checkScreen() {
   }
   document.getElementsByClassName("containers")[0].style.visibility = "visible";
   clearCTX();
-
   return true;
 }
-
 setTimeout(() => {
   btn_start = document.getElementById("btn_start");
   btn_continue = document.getElementById("btn_continue");
@@ -321,10 +290,9 @@ setTimeout(() => {
   });
   checkScreen();
 }, 1000);
-
 function ChecklineWidth() {
   if (x1_speed >= 32) {
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 4;
   }
   if (x1_speed < 32) {
     ctx.lineWidth = 3;
--- js/svg.js
+++ js/svg.js
@@ -3,13 +3,11 @@ jQuery("img.svg").each(function () {
   var imgID = $img.attr("id");
   var imgClass = $img.attr("class");
   var imgURL = $img.attr("src");
-
   jQuery.get(
     imgURL,
     function (data) {
       // Get the SVG tag, ignore the rest
       var $svg = jQuery(data).find("svg");
-
       // Add replaced image's ID to the new SVG
       if (typeof imgID !== "undefined") {
         $svg = $svg.attr("id", imgID);
@@ -18,15 +16,12 @@ jQuery("img.svg").each(function () {
       if (typeof imgClass !== "undefined") {
         $svg = $svg.attr("class", imgClass + " replaced-svg");
       }
-
       // Remove any invalid XML tags as per http://validator.w3.org
       $svg = $svg.removeAttr("xmlns:a");
-
       // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
       if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
         $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"));
       }
-
       // Replace image with new SVG
       $img.replaceWith($svg);
     },
